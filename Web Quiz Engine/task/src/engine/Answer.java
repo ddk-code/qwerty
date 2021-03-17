8adcf72520ba635f89b1443cd73696ba47f8d5d0
pcharlot <pcharlot@student.21-school.ru>
2021-03-17 14:34:38 +0300

    sdl

--- Web Quiz Engine/task/src/engine/Answer.java
+++ Web Quiz Engine/task/src/engine/Answer.java
@@ -1,6 +1,18 @@
+
 package engine;
 
 public class Answer {
-    private int chosenOption;
+    private int answer;
 
+    public Answer(int answer) {
+        this.answer = answer;
+    }
+
+    public int getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(int answer) {
+        this.answer = answer;
+    }
 }
--- Web Quiz Engine/task/src/engine/QuizController.java
+++ Web Quiz Engine/task/src/engine/QuizController.java
@@ -2,48 +2,6 @@ package engine;
 
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-
-
-
-
-//@RestController
-//@RequestMapping("/payment")
-//public class PaymentController {
-//
-//    private final String sharedKey = "SHARED_KEY";
-//
-//    private static final String SUCCESS_STATUS = "success";
-//    private static final String ERROR_STATUS = "error";
-//    private static final int CODE_SUCCESS = 100;
-//    private static final int AUTH_FAILURE = 102;
-//
-//    @GetMapping
-//    public BaseResponse showStatus() {
-//        return new BaseResponse(SUCCESS_STATUS, 1);
-//    }
-//
-//    @PostMapping("/pay")
-//    public BaseResponse pay(@RequestParam(value = "key") String key, @RequestBody PaymentRequest request) {
-//
-//        final BaseResponse response;
-//
-//        if (sharedKey.equalsIgnoreCase(key)) {
-//            int userId = request.getUserId();
-//            String itemId = request.getItemId();
-//            double discount = request.getDiscount();
-//            // Process the request
-//            // ....
-//            // Return success response to the client.
-//            response = new BaseResponse(SUCCESS_STATUS, CODE_SUCCESS);
-//        } else {
-//            response = new BaseResponse(ERROR_STATUS, AUTH_FAILURE);
-//        }
-//        return response;
-//    }
-//}
-
-
 
 @RestController
 @RequestMapping("/api/quiz")
@@ -53,16 +11,16 @@ public class QuizController {
     public Quiz getQuiz() {
         String title = "The Java Logo";
         String text = "What is depicted on the Java logo?";
-        String[] options = {"Robot","Tea leaf","Cup of coffee","Bug"};
+        String[] options = {"Robot", "Tea leaf", "Cup of coffee", "Bug"};
         return new Quiz(title, text, options);
     }
 
     @PostMapping
-    public boolean putAnswer(@RequestBody int index) {
-        if (index == 2) {
-            return true;
+    public Result putAnswer(@RequestBody int answer) {
+        if (answer == 2) {
+            return new Result(true, "Congratulations, you're right!");
         }
-        return false;
+        return new Result(false,"Wrong answer! Please, try again.");
     }
 
 }
